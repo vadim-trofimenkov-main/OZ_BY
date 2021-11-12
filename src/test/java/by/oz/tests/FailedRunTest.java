aefe55db7e9c617a5e8a15c4a95e90f197e85873
vadim-trofimenkov-main <vadimtrofimenkov97@gmail.com>
2021-11-12 15:30:30 +0300

    retry for cucumber

---
+++ src/test/java/by/oz/tests/FailedRunTest.java
@@ -0,0 +1,22 @@
+package by.oz.tests;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"},
+        monochrome = true,
+        features = {"@target/rerun.txt"},
+        glue = {"parallel"}
+)
+
+public class FailedRunTest extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
