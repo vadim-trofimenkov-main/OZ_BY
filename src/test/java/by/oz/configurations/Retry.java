a76df9632bb5e71b639c36094da93376a89deddc
Vadzim.Trafimenkau <Vadzim.Trafimenkau@itechart-group.com>
2022-06-16 13:52:16 +0300

    - Retry - добавил счетчик попытки - bitbucket-pipelines.yml - убрал игнор фейла и изменил путь артифактов

--- src/test/java/by/oz/configurations/Retry.java
+++ src/test/java/by/oz/configurations/Retry.java
@@ -16,7 +16,7 @@ public class Retry implements IRetryAnalyzer {
             if (attempt < MAX_RETRY) {
                 attempt++;
                 iTestResult.setStatus(ITestResult.FAILURE);
-                log.info("Retrying once again");
+                log.info("Retrying once again, {} attempt", attempt);
                 return true;
             } else {
                 iTestResult.setStatus(ITestResult.FAILURE);
